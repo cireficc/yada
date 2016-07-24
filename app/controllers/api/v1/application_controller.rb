a95d1e44b167a523d8b724b887b6c205efcac394
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-24 20:42:18 +0000

    Comment

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,6 +6,8 @@ module Api
   end
 end
 
+# Devise Token Auth can't find scoped classes, so give it a root-level class
+# inside the scoped API directory as a work-around
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 end
